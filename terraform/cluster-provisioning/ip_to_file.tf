73e2f25165cbeba00c497ef3fa415022e393f25d
Benjamin Catouillard <catouillard.benjamin@gmail.com>
2024-04-15 16:40:59 +0200

    refacto(terraform): remove cluster part

--- terraform/cluster-provisioning/ip_to_file.tf
+++ terraform/cluster-provisioning/ip_to_file.tf
@@ -1,8 +1,6 @@
 resource "local_file" "ansible_inventory" {
   content = templatefile("../../hosts/host.tmpl",
     {
-      ansible_group_cluster       = [for inst in google_compute_instance.default : inst.name],
-      hostname_cluster            = [for inst in google_compute_instance.default : "${google_compute_address.static_ip[inst.name].address} ansible_user=${split("@", google_service_account.default.email)[0]} ansible_ssh_common_args='-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null' ansible_ssh_private_key_file=${local_file.ssh_private_key_pem.filename}"],
       ansible_group_controlplanes = [for inst in google_compute_instance.default : inst.name if inst.labels["ansible-group"] == "controlplane"],
       hostname_controlplanes      = [for inst in google_compute_instance.default : "${google_compute_address.static_ip[inst.name].address} ansible_user=${split("@", google_service_account.default.email)[0]} ansible_ssh_common_args='-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null' ansible_ssh_private_key_file=${local_file.ssh_private_key_pem.filename}" if inst.labels["ansible-group"] == "controlplane"],
       ansible_group_nodes         = [for inst in google_compute_instance.default : inst.name if inst.labels["ansible-group"] == "node"],
